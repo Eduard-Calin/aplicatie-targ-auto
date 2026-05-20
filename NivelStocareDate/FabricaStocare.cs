bd0bc4724e51aefbbcc1a26fd0d366b70ae3f481
Comarita Eduard Călin <eduard.comarita@student.usv.ro>
2026-05-20 03:38:24 -0700

    Add files via upload

---
+++ NivelStocareDate/FabricaStocare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NivelStocareDate
+{
+    public static class FabricaStocare
+    {
+        public static IStocareTranzactii GetBazaDate()
+        {
+            // Folosim direct stocarea în memorie pentru acest stadiu al aplicației
+            return new StocareMemorie();
+        }
+    }
+}
