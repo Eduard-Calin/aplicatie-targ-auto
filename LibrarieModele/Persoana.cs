b450ef696e255b5ffbd6ca7d64e491a5adbea3d5
Comarita Eduard Călin <eduard.comarita@student.usv.ro>
2026-05-26 11:20:41 -0700

    Add files via upload

---
+++ LibrarieModele/Persoana.cs
@@ -0,0 +1,14 @@
+﻿namespace LibrarieModele
+{
+    public class Persoana
+    {
+        public string Nume { get; set; } = string.Empty;
+
+        public Persoana() { }
+
+        public Persoana(string nume)
+        {
+            Nume = nume ?? string.Empty;
+        }
+    }
+}
